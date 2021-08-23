f7110e642d3b2d6eb9a5bf81bb150e987c559147
amir <iamamirmostafa@gmail.com>
2021-08-23 10:52:06 +0600

    update

---
+++ 01.variable.js
@@ -0,0 +1,7 @@
+// JS is case sensitive
+// that mean myVariable !== MyVariable
+
+var firstName = "mostafa";
+var lastName = "mahmud";
+
+console.log(firstName + ' ' + lastName)
---
+++ 02.data-type.js
@@ -0,0 +1,22 @@
+// ## kind of data in JS
+// 1. Primitive Data 
+// 2. Non Primitive Data
+
+//## Primitive data
+1. Number
+ var mark = 10;
+2. String
+ var name = 'mostafa'
+3. Boolean
+ var isTeam = true
+ var isDone = false
+4. undefined 
+ var myName;
+5. null 
+ var something = null;
+
+//## Non Primitive
+1. Array 
+2. Object 
+3. function 
+
