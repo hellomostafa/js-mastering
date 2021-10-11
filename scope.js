c5119e320e0f5c1f05e085f0378b9a6a36cf3566
mostafa <iamamirmostafa@gmail.com>
2021-10-11 23:35:16 +0600

    update scope

--- scope.js
+++ scope.js
@@ -1,9 +1,50 @@
-for(var i = 0; i < 5; i++){
+// an Interview question.
+// for(var i = 0; i < 5; i++){
 
-    setTimeout(() => {
-        console.log(i)
-    },  1000);
+//     setTimeout(() => {
+//         console.log(i)
+//     },  1000);
     
+// }
+
+
+//var x = 23;
+//parent doniya
+function myFunc(){
+    // myFunc/child doniya
+    var x = 10;
+    console.log(`${x} from myFunc()`)
 }
+
+myFunc()
+
+
+
+console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ vlc.js
@@ -0,0 +1,12 @@
+if(true){
+    var varVariable = 'This is var';
+}
+
+console.log(varVariable)
+
+
+if(true){
+    let letVariable = 'This is let'
+}
+
+console.log(letVariable)
