7e740f3ed6c0413668e2dc8bd7353cf152e9e5bc
iammostafa76 <iamamirmostafa@gmail.com>
2021-10-31 09:26:28 +0600

    math

---
+++ All You Need to Know/01.Basics/math.js
@@ -0,0 +1,19 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 4.532
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+
+
+console.log(Math.max(400, 500, 700))
+console.log(Math.min(400, 500, 100))
+
+console.log(Math.pow(2, 3))
+console.log(Math.pow(5, 2))
+
+console.log(Math.sqrt(9))
+
+console.log(Math.round(Math.random() * 50 + 1))
