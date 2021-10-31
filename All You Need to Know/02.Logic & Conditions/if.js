5337c5bc2c1d60d6de079d49cc08dae51b0dfefb
iammostafa76 <iamamirmostafa@gmail.com>
2021-10-31 10:01:24 +0600

    if else added

---
+++ All You Need to Know/02.Logic & Conditions/if.js
@@ -0,0 +1,16 @@
+var a = 10;
+var b = 20;
+
+if(a > b){
+    console.log(a + ' A is bigger')
+}else{
+    console.log( b + ' B is Bigger')
+}
+
+var n = 6;
+
+if(n % 2 === 0){
+    console.log(n + " is Even Number")
+}else {
+    console.log(n + " is Odd Number")
+}
