e81874a4c6b2f297f00f4377f07f823ada8616cd
iammostafa76 <iamamirmostafa@gmail.com>
2021-11-01 10:33:10 +0600

    array added

---
+++ All You Need to Know/04.Array/array.js
@@ -0,0 +1,8 @@
+
+// Array
+
+// Array is a Data Structure where we can organize multiple data under a single variable.
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(arr)
+console.log(arr.length)
---
+++ All You Need to Know/04.Array/traverse.js
@@ -0,0 +1,15 @@
+var arr = [4, 5, 6, 7, 8, 9, 12, 49]
+
+// for(var i = 0; i < arr.length; i++){
+//     //console.log(arr[i])
+//     arr[i] = arr[i] + 2
+//     console.log(arr[i])
+// }
+
+var sum = 0
+
+for(var i = 0; i< arr.length; i++){
+    sum += arr[i]
+    
+}
+console.log(sum)
