b2890703cc996cb4f428cb6cb4359f1b0d263c29
iammostafa76 <iamamirmostafa@gmail.com>
2021-11-16 12:53:23 +0600

    object added

---
+++ All You Need to Know/05.Object/iterate.js
@@ -0,0 +1,23 @@
+var person = {
+    name: 'Amir',
+    age: 23,
+    profession: 'Developer'
+}
+
+
+var obj = {
+    x: 40,
+    y: 60,
+    z: 75
+}
+
+// console.log('x' in obj)
+// console.log('a' in obj)
+// console.log('name' in person)
+// console.log(person.name)
+
+
+for(var i in obj){
+    //console.log(i)
+    console.log(i + ': ' + obj[i])
+}
