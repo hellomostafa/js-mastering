a0065f4796bb822ebdcfcadfaf9cb754d0ec2896
iammostafa76 <iamamirmostafa@gmail.com>
2021-11-18 13:09:32 +0600

    function added

--- All You Need to Know/06.Function/function.js
+++ All You Need to Know/06.Function/function.js
@@ -1,3 +1,9 @@
 var date = new Date()
 var day = date.toLocaleString() // Date and time
 console.log('Order placed on ' + day)
+
+function add(){
+    var a = 10;
+    var b = 20
+    console.log(a + b)
+}
