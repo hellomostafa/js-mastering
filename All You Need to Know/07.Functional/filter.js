f92be136887380d2308b4c9be922136bedd67ebf
iammostafa76 <iamamirmostafa@gmail.com>
2021-11-27 10:46:26 +0600

    filter updated

--- All You Need to Know/07.Functional/filter.js
+++ All You Need to Know/07.Functional/filter.js
@@ -1,2 +1,22 @@
-var arr = [5, 4, 3, 6, 3, 2, 7]
+var arr = [5, 4, 3, 6, 3, 2, 7, 11, 12]
 
+var filteredArr = arr.filter(function(value){
+    return value % 2 === 1
+})
+
+console.log(filteredArr)
+
+
+
+
+function myFilter(arr){
+    var newArr = []
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+console.log(myFilter(arr))
